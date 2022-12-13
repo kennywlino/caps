6d9d658bbf768e8e0399ef0a71d626b5225976ba
Kenny W. Lino <kennywlino@gmail.com>
2022-12-13 10:52:18 -0800

    Following class code review. Refactored handlers from index into new files. Adding tests.

--- vendor/index.js
+++ vendor/index.js
@@ -1,28 +1,14 @@
 'use strict';
 
 const eventPool = require('../eventPool');
-const Chance = require('chance');
-const chance = new Chance();
+
+const { generateOrder, thankDriver } = require('./handlers');
 
 eventPool.on('DELIVERED', thankDriver);
-
-function generateOrder(){
-    let payload = {
-        'store' : '1-206-flowers',
-        'orderId' : chance.guid(),
-        'customer': chance.name(),
-        'address': chance.address(),
-    };
-
-    console.log('Vendor: Order for pick-up');
-    eventPool.emit('PICKUP', payload);
-}
-
-function thankDriver(payload){
-    console.log('Vendor: Thank you for delivering to:', payload.customer);
-}
 
 setInterval(() => {
     console.log('------New Pickup Order-------');
     generateOrder();
 }, 5000);
+
+module.exports = { generateOrder, thankDriver }
